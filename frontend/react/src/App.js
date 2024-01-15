ca3081aea92c687c83bcc1c268b6a2239caf451b
alxa221 <alextakele16@gmail.com>
2024-01-15 11:31:57 +0300

    ui compoents

---
+++ frontend/react/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import FormSubmission from './components/form_submission';
+function App() {
+  return (
+    <div className="App">
+     <FormSubmission/>
+    </div>
+  );
+}
+
+export default App;
